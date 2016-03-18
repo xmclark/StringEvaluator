67d253f585b4ba0a0ff49e0555e423a7da1cfd61
Mackenzie Clark <mackenzieclark@protonmail.ch>
2016-03-18 14:03:44 -0700

    Create IAlgorithm interface
    - the IAlgorithm interface is extensible interface for different kinds of evaluators
    - the IAlgorithm could implement a parser, a cleaner, or a solver
    - the default data type is IList because will need to be looped through and modified

---
+++ StringEvaluator/IAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEvaluator
+{
+    public interface IAlgorithm
+    {
+        IList<string> Execute(IList<string> expression);
+    }
+}
