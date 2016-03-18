259f02aeac795318f0ce427a56acedc64d3605d0
Mackenzie Clark <mackenzieclark@protonmail.ch>
2016-03-18 14:04:56 -0700

    Create Evaluator class
    - interfaces with IEvaluator
    - depends on IAlgorithm dependencies
    - the string is tokenized and then fed into the different IAlgorithms

---
+++ StringEvaluator/Evaluator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEvaluator
+{
+    public class Evaluator : IEvaluator
+    {
+        private IAlgorithm parser;
+        private IAlgorithm solver;
+        private IAlgorithm cleaner;
+
+        public Evaluator(IAlgorithm cleaner, IAlgorithm parser, IAlgorithm solver)
+        {
+            this.parser = parser;
+            this.solver = solver;
+            this.cleaner = cleaner;
+        }
+
+        public virtual string Evaluate(string expression)
+        {
+            IList<string> tokens = Regex.Matches(expression, @"([A-Za-z])+|([0-9])+|([\\\]\[\/\.\,\!\@\#\$\%\^\&\*\+\-]){1}")
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToList<string>();
+
+            var result = solver.Execute(parser.Execute(cleaner.Execute(tokens)));
+
+            StringBuilder resultString = new StringBuilder();
+            foreach (string token in result)
+            {
+                resultString.Append(token);
+            }
+
+            return resultString.ToString();
+        }
+    }
+}
