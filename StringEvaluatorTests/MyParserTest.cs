df085d7b89d0436fa26badf276a7cbb250bab94b
Mackenzie Clark <mackenzieclark@protonmail.ch>
2016-03-18 17:11:34 -0700

    Add a test for parenthesis expresssions

--- StringEvaluatorTests/MyParserTest.cs
+++ StringEvaluatorTests/MyParserTest.cs
@@ -25,7 +25,6 @@ namespace StringEvaluatorTests
         }
     }
     
-
     [TestClass]
     public class MyParserTest
     {
@@ -57,30 +56,35 @@ namespace StringEvaluatorTests
         }
 
         [TestMethod]
-        public void ParseInfixExpression_ExpectBadResult_GetError()
+        public void ParseInfixExpression_AppropriateResultsWithParenthesis()
         {
-            var expression1 = new List<string>();
-            var expected1 = new List<string>() { { "5" }, { "2" }, { "3" }, { "/" }, { "+" } };
-            expression1.Add("5");
-            expression1.Add("+");
-            expression1.Add("3");
-            expression1.Add("/");
-            expression1.Add("2");
+            var expression3 = new List<string>();
+            var expected3 = new List<string>() { { "1" }, { "2" }, { "3" }, { "*" }, { "+" } };
+            
+            expression3.Add("1");
+            expression3.Add("+");
+            expression3.Add("(");
+            expression3.Add("2");
+            expression3.Add("*");
+            expression3.Add("3");
+            expression3.Add(")");
 
-            var expression2 = new List<string>();
-            var expected2 = new List<string>() { { "5" }, { "3" }, { "+" }, { "2" }, { "/" } };
-            expression2.Add("5");
-            expression2.Add("/");
-            expression2.Add("3");
-            expression2.Add("+");
-            expression2.Add("2");
+            var expression4 = new List<string>();
+            var expected4 = new List<string>() { { "1" }, { "2" }, { "+" }, { "3" }, { "*" } };
+            expression4.Add("(");
+            expression4.Add("1");
+            expression4.Add("+");
+            expression4.Add("2");
+            expression4.Add(")");
+            expression4.Add("*");
+            expression4.Add("3");
 
             var myParser = new MyParser();
-            var result1 = new List<string>(myParser.Execute(expression1));
-            var result2 = new List<string>(myParser.Execute(expression2));
+            var result3 = new List<string>(myParser.Execute(expression3));
+            var result4 = new List<string>(myParser.Execute(expression4));
 
-            Assert.IsFalse(ShallowListEquality.Test(expected1, result1));
-            Assert.IsFalse(ShallowListEquality.Test(expected2, result2));
+            Assert.IsTrue(ShallowListEquality.Test(expected3, result3));
+            Assert.IsTrue(ShallowListEquality.Test(expected4, result4));
         }
     }
 }
