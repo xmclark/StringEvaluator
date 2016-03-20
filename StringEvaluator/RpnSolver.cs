9025e8a8f4b526704c011942e99f39cc724a876a
Mackenzie Clark <mackenzieclark@protonmail.ch>
2016-03-19 17:04:58 -0700

    Add Rpn solver class
    - inherits from IAlgorithm interface
    - This is a standard algorithm for evalauting a postifx expression in a list form

---
+++ StringEvaluator/RpnSolver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace StringEvaluator
+{
+    public class RpnSolver : IAlgorithm
+    {
+        private Stack<decimal> stack;
+
+        private List<string> result;
+
+        public IList<string> Execute(IList<string> expression)
+        {
+            stack = new Stack<decimal>();
+            result = new List<string>();
+
+            foreach (string token in expression)
+            {
+                var valueMatch = Regex.Match(token, @"\d+");
+                var isValue = valueMatch.Success;
+
+                var operatorMatch = Regex.Match(token, @"[\+\-\*\/\^]");
+                var isOperator = operatorMatch.Success;
+
+                if (isValue)
+                {
+                    stack.Push(Decimal.Parse(token));
+                }
+
+                else if (isOperator)
+                {
+                    decimal A = stack.Pop();
+                    decimal B = stack.Pop();
+
+                    if (token == "+") stack.Push(A + B);
+                    else if (token == "-") stack.Push(B - A);
+                    else if (token == "*") stack.Push(A * B);
+                    else if (token == "/") stack.Push(B / A);
+                    else if (token == "^") stack.Push((decimal)Math.Pow((double)B, (double)A));
+                }
+            }
+
+            result.Add(stack.Pop().ToString());
+
+            return result;
+        }
+
+        public RpnSolver()
+        {
+        }
+
+    }
+}
