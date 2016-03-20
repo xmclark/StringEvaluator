0b6f55c917d130c8ce0b91d5989e60d190f04dcc
Mackenzie Clark <mackenzieclark@protonmail.ch>
2016-03-20 14:45:15 -0700

    Move ShallowListEquality tester class into own file

--- StringEvaluatorTests/MyParserTest.cs
+++ StringEvaluatorTests/MyParserTest.cs
@@ -4,25 +4,6 @@ using StringEvaluator;
 
 namespace StringEvaluatorTests
 {
-    public class ShallowListEquality
-    {
-        public static bool Test(List<string> List1, List<string> List2)
-        {
-            if (List1.Count == List2.Count)
-            {
-                for (int i = 0; i < List1.Count; i++)
-                {
-                    if (List1[i] != List2[i])
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return false;
-        }
-    }
-    
     [TestClass]
     public class MyParserTest
     {
