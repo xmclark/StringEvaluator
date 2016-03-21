2109297c3584d43e36095df9794f0416736d426a
xmclark <mackenzieclark@protonmail.ch>
2016-03-20 18:16:58 -0700

    Add example of using evaluator using my algorithm

--- StringEvaluator/EntryPoint.cs
+++ StringEvaluator/EntryPoint.cs
@@ -6,6 +6,16 @@ namespace StringEvaluator
     {
         static void Main()
         {
+            var solver = new RpnSolver();
+            var validator = new ExpressionValidator();
+            var parser = new MyParser();
+
+            var evaluator = new Evaluator(validator, parser, solver);
+
+            Console.WriteLine(evaluator.Evaluate("1+2"));
+            Console.WriteLine(evaluator.Evaluate("(1+2)*3+(4/7)"));
+            Console.WriteLine(evaluator.Evaluate("(58)/12"));
+
         }
     }
 }
